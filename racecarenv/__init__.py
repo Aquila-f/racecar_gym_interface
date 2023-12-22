7b91758dcd75ff2ceee8c7ea6ea5637868c3ff44
Aquila <qwertgb1234@gmail.com>
2023-12-22 11:06:21 +0800

    saveInfowrapper bug fix

--- racecarenv/__init__.py
+++ racecarenv/__init__.py
@@ -4,10 +4,11 @@ from .rewardshape import get_rewardshaping_wrapper
 
 
 def get_env(args, conf, **kwargs):
+    if args.name and 'SaveInfoWrapper' in conf["wrappers"]: conf["wrappers"]["SaveInfoWrapper"]["save_pos_folder_name"] = args.name
     env = RaceEnv(scenario=args.scenario,
                   reset_when_collision=False if 'collisionStop' in args.scenario else True,
                   **kwargs,)
-    env = get_wrapper(env, conf["wrappers"])
+    env = get_wrapper(env, conf["wrappers"], args.eport)
     env = get_rewardshaping_wrapper(env, conf["rewardfuncs"])
     return env
                 
